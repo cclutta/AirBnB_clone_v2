3c3dc08b2c19645fcaf0c62acd1b8b8bb1c37c32
cclutta <chelsea.lutta@gmail.com>
2022-09-14 21:03:33 +0300

    Update

--- models/user.py
+++ models/user.py
@@ -12,16 +12,7 @@ class User(BaseModel, Base):
 
     __tablename__ = "users"
 
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", cascade="all, delete", backref="user")
-        review = relationship("Review", cascade="all, delete", backref="user")
-
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
