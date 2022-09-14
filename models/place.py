cf12c074d22b333ac8537afd50a11319f9d016c6
cclutta <chelsea.lutta@gmail.com>
2022-09-14 21:42:45 +0300

    Update

--- models/place.py
+++ models/place.py
@@ -8,6 +8,14 @@ from models.amenity import Amenity
 from sqlalchemy import Column, Table, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 
+place_amenity = Table("place_amenity", Base.metadata,
+                      Column("place_id", String(60),
+                             ForeignKey("places.id"),
+                             primary_key=True),
+                      Column("amenity_id", String(60),
+                             ForeignKey("amenities.id"),
+                             primary_key=True))
+
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -25,6 +33,9 @@ class Place(BaseModel, Base):
     latitude = Column(Float)
     longitude = Column(Float)
     reviews = relationship("Review", cascade="all, delete", backref="place")
+    amenities = relationship("Amenity", secondary=place_amenity,
+                             backref="place_amenities",
+                             viewonly=False)
 
     @property
     def reviews(self):
@@ -35,3 +46,19 @@ class Place(BaseModel, Base):
             if review.place_id == self.id:
                 lst.append(review)
             return lst
+
+    @property
+    def amenities(self):
+        """Amenities getter"""
+        amenities = models.storage.all(Amenity)
+        lst = []
+        for amenity in amenities.values():
+            if amenity.id in self.amenity_ids:
+                lst.append(amenity)
+        return lst
+
+    @amenities.setter
+    def amenities(self, obj):
+        """Amenities setter"""
+        if type(obj) == Amenity:
+            self.amenity_ids.append(obj.id)
