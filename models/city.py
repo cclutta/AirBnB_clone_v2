098405e1ee460e58e939e57a9b8f03d0ff2094a7
cclutta <chelsea.lutta@gmail.com>
2022-09-14 21:00:33 +0300

    Update

--- models/city.py
+++ models/city.py
@@ -10,12 +10,5 @@ class City(BaseModel, Base):
     """ The city class, contains state ID and name """
 
     __tablename__ = "cities"
-
-    if getenv('HBNB_TYPE_STORAGE') == "db":
-        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-        name = Column(String(128), nullable=False)
-        places = relationship("Place", cascade="all, delete", backref="cities")
-
-    else:
-        state_id = ""
-        name = ""
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(128), nullable=False, ForeignKey("states.id"))
--- models/state.py
+++ models/state.py
@@ -12,21 +12,15 @@ class State(BaseModel, Base):
     """ State class """
 
     __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade="all, delete", backref="state")
 
-    if getenv('HBNB_TYPE_STORAGE') == "db":
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", cascade="all, delete", backref="state")
-
-    else:
-        name = ""
-
-        @property
-        def cities(self):
-            """ Returns the list of City instances with
-            state_id equals to the current State.id """
-            cities = models.storage.all(City)
-            lst = []
-            for city in cities.values():
-                if city.state_id == self.id:
-                    lst.append(city)
-            return lst
+    @property
+    def cities(self):
+        """ Returns the list of City instances with state_id"""
+        cities = models.storage.all(City)
+        lst = []
+        for city in cities.values():
+            if city.state_id == self.id:
+                lst.append(city)
+        return lst
