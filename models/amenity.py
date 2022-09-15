302d1cf15657a3bfa5cd82084a51a612d6bf3b41
cclutta <chelsea.lutta@gmail.com>
2022-09-15 11:34:43 +0300

    Update

--- models/amenity.py
+++ models/amenity.py
@@ -2,9 +2,9 @@
 """ State Module for HBNB project """
 from os import getenv
 from models.base_model import BaseModel, Base
-# import models
+import models
 from sqlalchemy import Column, String
-# from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
@@ -12,4 +12,4 @@ class Amenity(BaseModel, Base):
 
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity)
+    place_amenities = relationship("Place", secondary="place_amenity")
