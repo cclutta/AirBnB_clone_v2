8ac9a9d33a10eeb8ce4f241c83c2fd7d2d3f2f03
cclutta <chelsea.lutta@gmail.com>
2022-10-16 13:29:27 +0300

    Update

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Flask c_route script. """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+       Flask route at root.
+       Displays Hello BNB!
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+       Flask route at /hbnb.
+       Displays BNB!
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """
+       Flask route at /c/<text>.
+       Displays C + text
+    """
+    return "C {}".format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
