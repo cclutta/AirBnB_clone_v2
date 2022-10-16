d4ab7885acf3c7e8b3c321b4da2a0ed8f2d4f347
cclutta <chelsea.lutta@gmail.com>
2022-10-16 13:39:59 +0300

    Update

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -31,6 +31,7 @@ def c_text(text):
     """
     return "C {}".format(text.replace('_', ' '))
 
+
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text="is cool"):
